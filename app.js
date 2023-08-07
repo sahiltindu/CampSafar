abcdc56c4ca480ca4688b4c18253479fb9cf1688
sahil <ss2323155@gmail.com>
2023-08-08 02:04:12 +0530

    step-3

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
-
+const Review = require('./models/review');
 mongoose.connect('mongodb://0.0.0.0:27017/yelp-camp', {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -80,6 +80,15 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
     res.redirect('/campgrounds');
+}))
+app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect('/campgrounds/${campground._id}');
+
 }))
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page not found', 404))
