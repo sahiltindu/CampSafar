44a00f3c426be40d80eaecc3c85f1df4ec654ddd
sahil <ss2323155@gmail.com>
2023-08-13 16:47:27 +0530

    step-13

--- models/campground.js
+++ models/campground.js
@@ -8,7 +8,7 @@ const ImageSchema = new Schema({
 ImageSchema.virtual('thumbnail').get(function () {
     return this.url.replace('/upload', '/upload/w_200');
 });
-
+const opts = { toJSON: { virtuals: true } };
 const CampgroundSchema = new Schema({
     title: String,
     images: [ImageSchema],
@@ -36,7 +36,14 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+    
+},opts);
+CampgroundSchema.virtual('properties.popUpMarkup').get(function () {
+    return `
+    <strong><a href="/campgrounds/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0, 20)}...</p>`
 });
+
 
 CampgroundSchema.post('findOneAndDelete', async function (doc) {
     if (doc)
