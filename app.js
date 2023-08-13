9ab525c3f53571ef4c25488ce932a9de777ed699
sahil <ss2323155@gmail.com>
2023-08-14 02:25:11 +0530

    step-16

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const MongoDBStore = require("connect-mongo")(session);
-const dbUrl = 'mongodb://0.0.0.0:27017/yelp-camp'
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
